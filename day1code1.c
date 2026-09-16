cba434c9661f50daa9cba86aa50c98a0a980e70d
chauhanjix9-tech <chauhanjix9@gmail.com>
2026-09-16 13:19:36 +0530

    Add files via upload

---
+++ day1code1.c
@@ -0,0 +1,12 @@
+//Write a program to input two numbers and display their sum. 
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    int b = 20;
+    int sum = a + b;
+
+    printf("Sum = %d", sum);
+
+    return 0;
+}
